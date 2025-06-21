71ea4ad532a9ec0acfb9b3979d1af094f8394394
benjo00 <130617538+benjo00@users.noreply.github.com>
2025-06-21 14:11:46 -0500

    add validation tolowercase, etc to search component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,19 +2,28 @@ import React from 'react';
 import data from './MockData.jsx';
 
 function Search({ input }) {
-  const filteredData = data.filter((item) => {
-    if (input === '') return true;
-    return item.text.toLowerCase().includes(input);
-  });
+  const cleanedInput = input.trim().toLowerCase();
 
+  const filteredData = data.filter((item) => {
+    if (cleanedInput === '') return true;
+    return item.text.toLowerCase().includes(cleanedInput);
+  });
+// This adds validation as per project requested requirements. Shows "No results found." It also utilizes the trim and tolowercase functionality in order to ensure the search function works regardless of user input outside of these
   return (
-    <ul>
-      {filteredData.map((item) => (
-        <li key={item.id}>{item.text}</li>
-      ))}
-    </ul>
+    <div>
+      <ul>
+        {filteredData.length > 0 ? (
+          filteredData.map((item) => (
+            <li key={item.id}>{item.text}</li>
+          ))
+        ) : (
+          <li>No results found.</li>
+        )}
+      </ul>
+    </div>
   );
 }
 
-export default Search ;
+export default Search;
+
 // the above creates the searchbar function and maps the data from the mockdata files
