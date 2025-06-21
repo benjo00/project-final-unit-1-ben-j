d83c32c8dad4281ef35dc99a686b9c4e6ed29ff8
benjo00 <130617538+benjo00@users.noreply.github.com>
2025-06-21 11:54:03 -0500

    .

--- src/components/modern.jsx
+++ src/components/modern.jsx
@@ -39,69 +39,7 @@ function Modern() {
         ))}
       </ul>
 
-      <h3>Study Highlights</h3>
-      <table>
-        <thead>
-          <tr>
-            <th>Study Title</th>
-            <th>Topic</th>
-            <th>Year</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>The Science of Meditation</td>
-            <td>General Benefits</td>
-            <td>2021</td>
-          </tr>
-          <tr>
-            <td>Mindfulness of breathing, how does it affect cardio metabolic health?</td>
-            <td>Cardiovascular Health</td>
-            <td>2020</td>
-          </tr>
-          <tr>
-            <td>Does walking meditation help anxiety? A long term study</td>
-            <td>Mental Health</td>
-            <td>2019</td>
-          </tr>
-          <tr>
-            <td>Is Yoga good for arthritis?</td>
-            <td>Joint Health</td>
-            <td>2018</td>
-          </tr>
-          <tr>
-            <td>How important is stretching for elderly meditators?</td>
-            <td>Aging</td>
-            <td>2022</td>
-          </tr>
-          <tr>
-            <td>Zen, a 20 year study</td>
-            <td>Philosophy</td>
-            <td>2015</td>
-          </tr>
-          <tr>
-            <td>Surya Namaskar for joint mobility in older adults</td>
-            <td>Mobility</td>
-            <td>2021</td>
-          </tr>
-          <tr>
-            <td>Sun Salutations as a method of helping people get more exercise, a study</td>
-            <td>Fitness</td>
-            <td>2017</td>
-          </tr>
-          <tr>
-            <td>A 30 year study on the benefits of vipassana for stress levels</td>
-            <td>Stress Reduction</td>
-            <td>2023</td>
-          </tr>
-          <tr>
-            <td>Mindfulness in daily life compared to control, a meta study</td>
-            <td>Daily Practice</td>
-            <td>2020</td>
-          </tr>
-        </tbody>
-      </table>
-      {/* Added a table using my mock data as per final project requirements for including a table*/}
+      
     </div>
   );
 }
